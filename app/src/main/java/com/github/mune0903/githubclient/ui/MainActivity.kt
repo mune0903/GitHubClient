a2834c8053a5dd20843faa2c91f50894e62705e1
mune0903 <b1016051@fun.ac.jp>
2019-05-26 22:38:21 +0900

    fragmentをセット

--- app/src/main/java/com/github/mune0903/githubclient/ui/MainActivity.kt
+++ app/src/main/java/com/github/mune0903/githubclient/ui/MainActivity.kt
@@ -9,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Fragmentをセット
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, MainFragment.newInstance(), MainFragment.TAG)
+            .commit()
     }
 }
--- app/src/main/java/com/github/mune0903/githubclient/ui/MainFragment.kt
+++ app/src/main/java/com/github/mune0903/githubclient/ui/MainFragment.kt
@@ -3,4 +3,12 @@ package com.github.mune0903.githubclient.ui
 import androidx.fragment.app.Fragment
 
 class MainFragment : Fragment() {
+
+    companion object {
+
+        val TAG: String = MainFragment::class.java.simpleName
+
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
 }
