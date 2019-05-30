4be47f5716f00c6852db0bbd67ad8d66c8c2c0be
mune0903 <b1016051@fun.ac.jp>
2019-05-30 15:34:04 +0900

    fix

--- app/src/main/java/com/github/mune0903/githubclient/ui/login/LoginFragment.kt
+++ app/src/main/java/com/github/mune0903/githubclient/ui/login/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.github.mune0903.githubclient.OAUTH_URL
 import com.github.mune0903.githubclient.R
 import com.github.mune0903.githubclient.databinding.FragmentLoginBinding
 import com.github.mune0903.githubclient.util.factory.ViewModelFactory
@@ -22,9 +23,6 @@ class LoginFragment : Fragment() {
         super.onCreate(savedInstanceState)
         val viewModelFactory = ViewModelFactory()
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(LoginViewModel::class.java)
-        viewModel.oauthUrl.observe(this, Observer {
-            binding.webView.loadUrl(it.toString())
-        })
     }
 
     override fun onCreateView(
@@ -43,7 +41,10 @@ class LoginFragment : Fragment() {
     }
 
     private fun setupWebView() {
-        binding.webView.webViewClient = WebViewClient()
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(OAUTH_URL)
+        }
     }
 
     companion object {
