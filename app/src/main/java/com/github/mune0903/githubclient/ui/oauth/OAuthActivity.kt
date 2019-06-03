060f4a47fef5ff59fd0a420947466f9f8398e9de
mune0903 <b1016051@fun.ac.jp>
2019-06-03 22:29:22 +0900

    fix: navigation

--- app/src/main/java/com/github/mune0903/githubclient/ui/oauth/OAuthActivity.kt
+++ app/src/main/java/com/github/mune0903/githubclient/ui/oauth/OAuthActivity.kt
@@ -1,5 +1,6 @@
 package com.github.mune0903.githubclient.ui.oauth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mune0903.githubclient.R
--- app/src/main/java/com/github/mune0903/githubclient/ui/oauth/OAuthFragment.kt
+++ app/src/main/java/com/github/mune0903/githubclient/ui/oauth/OAuthFragment.kt
@@ -3,21 +3,16 @@ package com.github.mune0903.githubclient.ui.oauth
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.github.mune0903.githubclient.CLIENT_ID
 import com.github.mune0903.githubclient.OAUTH_URL
-import com.github.mune0903.githubclient.R
 import com.github.mune0903.githubclient.REDIRECT_URI
-import com.github.mune0903.githubclient.databinding.FragmentOauthBinding
 import com.github.mune0903.githubclient.util.factory.ViewModelFactory
 
-class OAuthFragment : Fragment() {
 
-    private lateinit var binding: FragmentOauthBinding
+class OAuthFragment : Fragment() {
 
     private lateinit var viewModel: OAuthViewModel
 
@@ -25,29 +20,19 @@ class OAuthFragment : Fragment() {
         super.onCreate(savedInstanceState)
         val viewModelFactory = ViewModelFactory()
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(OAuthViewModel::class.java)
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_oauth, container, false)
-        binding = FragmentOauthBinding.bind(view)
-        return binding.root
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse(OAUTH_URL + "?client_id=" + CLIENT_ID + "&scope=repo&redirect_uri=" + REDIRECT_URI)
+        )
+        startActivity(intent)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //setupWebView()
-        binding.oauthButton.setOnClickListener {
-            val intent = Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse(OAUTH_URL + "?client_id=" + CLIENT_ID + "&scope=repo&redirect_uri=" + REDIRECT_URI)
-            )
-            startActivity(intent)
-        }
+
     }
+
 
     companion object {
 
