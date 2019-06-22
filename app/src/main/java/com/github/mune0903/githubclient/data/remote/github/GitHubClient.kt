d64a6cc0d31a0db74b0b8a7aa8ba600a947fcc48
mune0903 <b1016051@fun.ac.jp>
2019-06-23 02:48:31 +0900

    add: module

--- app/src/main/java/com/github/mune0903/githubclient/data/remote/github/GitHubClient.kt
+++ app/src/main/java/com/github/mune0903/githubclient/data/remote/github/GitHubClient.kt
@@ -1,7 +1,6 @@
 package com.github.mune0903.githubclient.data.remote.github
 
 import com.github.mune0903.githubclient.data.model.News
-import com.github.mune0903.githubclient.data.model.Token
 import com.github.mune0903.githubclient.data.model.User
 import com.github.mune0903.githubclient.data.remote.*
 import io.reactivex.Observable
